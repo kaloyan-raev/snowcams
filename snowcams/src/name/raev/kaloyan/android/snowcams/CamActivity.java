608cd8f2b796a7a648297f19b6a1049232f8705a
I024147 <I024147@SOFN60221251A>
2012-01-21 00:14:16 +0300

    Cache image for reuse when orientation changes.

--- snowcams/src/name/raev/kaloyan/android/snowcams/CamActivity.java
+++ snowcams/src/name/raev/kaloyan/android/snowcams/CamActivity.java
@@ -5,7 +5,9 @@ import java.io.InputStream;
 import java.net.URL;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -28,41 +30,54 @@ public class CamActivity extends Activity {
         mProgressBar = (ProgressBar) findViewById(R.id.progress);
         mImageView = (ImageView) findViewById(R.id.camImage);
         
-        // show the progress bar before starting loading the image
-        mProgressBar.setVisibility(View.VISIBLE);
-        mImageView.setVisibility(View.INVISIBLE);
-        
-        new Thread(new Runnable() {
-			@Override
-			public void run() {
-				final Drawable drawable = loadImageFromNetwork();
-				mImageView.post(new Runnable() {
-					@Override
-					public void run() {
-						if (drawable == null) {
-							// no image was loaded
-							mImageView.setImageResource(R.drawable.no_camera);
-							mImageView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, Gravity.CENTER));
-						} else {
-							// set the loaded image
-							mImageView.setImageDrawable(drawable);
-							mImageView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, Gravity.CENTER));
+        // check if there is a cached image
+        Object cachedImage = getLastNonConfigurationInstance();
+        if (cachedImage != null) {
+        	mImageView.setImageBitmap((Bitmap) cachedImage);
+        } else {
+	        // show the progress bar before starting loading the image
+	        mProgressBar.setVisibility(View.VISIBLE);
+	        mImageView.setVisibility(View.INVISIBLE);
+	        
+	        // load the image in a separate thread
+	        new Thread(new Runnable() {
+				@Override
+				public void run() {
+					final Bitmap bitmap = loadImageFromNetwork();
+					mImageView.post(new Runnable() {
+						@Override
+						public void run() {
+							if (bitmap == null) {
+								// no image was loaded
+								mImageView.setImageResource(R.drawable.no_camera);
+								mImageView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+							} else {
+								// set the loaded image
+								mImageView.setImageBitmap(bitmap);
+								mImageView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, Gravity.CENTER));
+							}
+							// hide the progress bar
+							mImageView.setVisibility(View.VISIBLE);
+							mProgressBar.setVisibility(View.INVISIBLE);
 						}
-						// hide the progress bar
-						mImageView.setVisibility(View.VISIBLE);
-						mProgressBar.setVisibility(View.INVISIBLE);
-					}
-				});
-			}
-        }).start();
+					});
+				}
+	        }).start();
+        }
     }
+    
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		// cache the image
+		return ((BitmapDrawable) mImageView.getDrawable()).getBitmap();
+	}
 
-	private Drawable loadImageFromNetwork() {
+	private Bitmap loadImageFromNetwork() {
 		try {
 			URL url = new URL("http://cam.dir.bg/sf/c2/image.jpg");
 			InputStream is = (InputStream) url.getContent();
-			Drawable drawable = Drawable.createFromStream(is, null);
-			return drawable;
+			Bitmap bitmap = BitmapFactory.decodeStream(is);
+			return bitmap;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
